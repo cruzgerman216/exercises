897dd4f3f809344ff1734be097fd0e166b666e4e
German Cruz <43277807+cruzgerman216@users.noreply.github.com>
2022-09-22 20:18:19 -0500

    Exercise 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exercises';
+  name: string = "John"
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { PostsComponent } from './posts/posts.component';
-import { FriendsComponent } from './friends/friends.component';
-import { MyprofileComponent } from './myprofile/myprofile.component';
-import { FriendsListComponent } from './friends-list/friends-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    PostsComponent,
-    FriendsComponent,
-    MyprofileComponent,
-    FriendsListComponent,
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
